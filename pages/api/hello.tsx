35c12ae9ac3883ccf6dd8219733ea81c4716175e
Pradeep Gupta <pradeepngupta@gmail.com>
2025-01-11 11:10:38 +0530

    fixing warning on hello.tsx

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -5,6 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 type Data = {
 	name: string;
 };
-export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
-	res.status(200).json({ name: 'John Doe' });
-};
+function handler(req: NextApiRequest, res: NextApiResponse<Data>): void {
+	res.status(200).json({ name: 'Pradeep Gupta' });
+}
+export default handler;
