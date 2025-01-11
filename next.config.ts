d140754a85e2eb6964a7cd5c32af1a1afc0fbe0c
Pradeep Gupta <pradeepngupta@gmail.com>
2025-01-11 11:10:04 +0530

    eslint recommendation on nextjs

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	// Warning: This allows production builds to successfully complete even if
+	// your project has ESLint errors.
+	ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
