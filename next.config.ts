01c08c61f68acb8e406a19d0c85ad1701dabfea2
Pradeep Gupta <pradeepngupta@gmail.com>
2025-01-13 00:10:52 +0530

    fixing ci.yml

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	output: 'export',
 	// Warning: This allows production builds to successfully complete even if
 	// your project has ESLint errors.
 	eslint: {
