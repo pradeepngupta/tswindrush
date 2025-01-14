fb3a93c070c0d2d2c75ba5e82b00d7491d58d014
Pradeep Gupta <pradeepngupta@gmail.com>
2025-01-14 17:28:47 +0530

    SEO related changes to build Sitemap & robots.txt

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,24 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: true,
-	output: 'export',
 	// Warning: This allows production builds to successfully complete even if
 	// your project has ESLint errors.
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
 	images: { unoptimized: true },
+	async rewrites() {
+		return [
+			{
+				source: '/robots.txt',
+				destination: '/api/robots',
+			},
+			{
+				source: '/sitemap.xml',
+				destination: '/api/sitemap',
+			},
+		];
+	},
 };
 
 export default nextConfig;
