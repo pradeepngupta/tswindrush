a0d341716f0cffcc49e605854ab15e256abb9b9e
Pradeep Gupta <pradeepngupta@gmail.com>
2025-01-18 16:56:31 +0530

    jest config to show code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
 		],
 	},
 	collectCoverage: true,
-	collectCoverageFrom: ['./src/app/pages/**'],
+	collectCoverageFrom: ['./src/app/**'],
 	coverageThreshold: {
 		global: {
 			branches: 30,
