90e036d6205f5614530203a6192b6bdf38bd53cc
Pradeep Gupta <pradeepngupta@gmail.com>
2025-06-24 10:04:23 +0530

    Update version numbers and remove unused dependency

--- __e2e__/version.spec.ts
+++ __e2e__/version.spec.ts
@@ -6,13 +6,13 @@ test('GET /api/version should return the current version', async ({ request }) =
 	const responseBody = await response.json();
 
 	expect(responseBody).toHaveProperty('nextjs');
-	expect(responseBody.nextjs).toBe('15.3.3');
+	expect(responseBody.nextjs).toBe('15.4.0');
 
 	expect(responseBody).toHaveProperty('reactjs');
 	expect(responseBody.reactjs).toBe('19.1.0');
 
 	expect(responseBody).toHaveProperty('tailwindCSS');
-	expect(responseBody.tailwindCSS).toBe('4.1.8');
+	expect(responseBody.tailwindCSS).toBe('4.1.10');
 
 	expect(responseBody).toHaveProperty('typescript');
 	expect(responseBody.typescript).toBe('5.8.3');
